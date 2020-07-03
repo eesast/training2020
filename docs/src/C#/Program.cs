dbc769e34507432fe216e9809c8a54f184ee7932
Bowman Chow <bowmanchow@gmail.com>
2020-07-03 22:31:31 +0800

    add C# Learning 2, MultiThread

---
+++ docs/src/C#/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Event.Run();
+            Console.ReadKey();
+        }
+    }
+}
